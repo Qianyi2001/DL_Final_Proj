ac56c55b846f722dc3f8279fd8cb7772068e075d
unknown <qx690@nyu.edu>
2024-12-09 22:41:25 -0500

    Update

--- evaluator.py
+++ evaluator.py
@@ -153,6 +153,8 @@ class ProbingEvaluator:
                     target = sampled_target_locs.cuda()
 
                 pred_locs = torch.stack([prober(x) for x in pred_encs], dim=1)
+                print("pred_locs shape:", pred_locs.shape)
+                print("target shape:", target.shape)
                 losses = location_losses(pred_locs, target)
                 per_probe_loss = losses.mean()
 
